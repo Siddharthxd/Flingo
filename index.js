aa46d02576f05fdb02be1c32827b14ae3eef8f36
Siddharth_xd <167385150+Siddharthxd@users.noreply.github.com>
2024-06-04 14:27:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const wait = require('wait')
+require('dotenv').config()
+require('module-alias/register')
+const path = require('path')
+const Flingo = require(`./structures/Flingo.js`)
+const client = new Flingo()
+const GiveawayManager = require("./handler/GiveawayManager");
+
+client.giveawaysManager = new GiveawayManager(client);
+
+
+this.config = require(`${process.cwd()}/config.json`);
+(async () => {
+    await client.initializeMongoose()
+    await client.initializedata()
+    await wait(3000);
+    (await client.loadEvents()) - (await client.loadlogs())
+    await client.loadMain()
+    await client.login('token')
+
+})()
